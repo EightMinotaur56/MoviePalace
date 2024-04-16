59a17948fef91bf63759bc53826c58d980c376eb
EightMinotaur56 <sos.tomi9@gmail.com>
2024-04-16 10:44:07 +0200

    fix for router in app.js

--- moviepalace/client/src/App.js
+++ moviepalace/client/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
-import Login from './Pages/login'; 
+import Login from './Pages/login';
 import Register from './Pages/register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ForgotPass from './Pages/forgotten-password';
 
 function App() {
   return (
-    <Router>
       <div className="app-container">
-        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -18,7 +16,6 @@ function App() {
           <Route path="/forgotten-password" element={<ForgotPass />} />
         </Routes>
       </div>
-    </Router>
   );
 }
 
