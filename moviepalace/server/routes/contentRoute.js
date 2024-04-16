ee4da1301132e7d946890e4b40de27a1518b02a4
EightMinotaur56 <sos.tomi9@gmail.com>
2024-04-16 10:48:58 +0200

    removed unised files

--- moviepalace/server/routes/contentRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getAllMovies, getMovieById} = require('../controllers/contentController');
-
-router.get('/', getAllMovies); //back at controller
-router.get('/:id', getMovieById); //back at controller
-
-module.exports = router;
