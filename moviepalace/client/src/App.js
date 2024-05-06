142fba10ecfdbbc0dca02524d1fddfb4bee510ee
Dalany28 <dalanyblmv@gmail.com>
2024-05-06 18:29:36 +0200

    ddd
    c

--- moviepalace/client/src/App.js
+++ moviepalace/client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './Pages/Home';
 import Login from './Pages/login';
 import Register from './Pages/register'
+import MovieScreen from './Pages/MovieScreen';
+import Confirmation from './Pages/Confirmation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ForgotPass from './Pages/forgotten-password';
 
@@ -14,6 +16,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgotten-password" element={<ForgotPass />} />
+          <Route path="/movie/:id" element={<MovieScreen />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+
+
         </Routes>
       </div>
   );
